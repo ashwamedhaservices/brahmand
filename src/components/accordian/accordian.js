e3dba3ed0ef655f43233e80402d20c2645057098
Ravindra Kumawat <ravindra.kumawat@kuvera.in>
2023-09-17 12:13:32 +0530

    Added the card indication for subscribed and unsubscribed

--- src/components/accordian/accordian.js
+++ src/components/accordian/accordian.js
@@ -1,8 +1,9 @@
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Typography, Accordion,  AccordionDetails, AccordionSummary } from "@mui/material";
-
+import VerifiedIcon from '@mui/icons-material/Verified';
+import GppMaybeIcon from '@mui/icons-material/GppMaybe';
 const mystyle = (subscribed) => ({
-  color: "var(--theme-font-color-secondary)",
+  color: subscribed === true ? "var(--theme-font-color-secondary)" : 'var(--theme-font-color-primary)',
   // height: '84px',
   backgroundColor: subscribed === true ? 'var(--theme-background-quaternary)' : 'var(--theme-background-tertiary)',
   borderRadius: "12px",
@@ -23,11 +24,14 @@ function CustomAccordian({ data, handleUserClick }) {
               id="panel1a-header"
             >
               <div className="k-flex k-fdr k-jcsb" style={{ width: "100%" }}>
-                <Typography onClick={() => handleUserClick(item)}>{item.name}</Typography>
+                <Typography onClick={() => handleUserClick(item)} className="k-flex k-aic">{item.name} {item.subscribed ? <VerifiedIcon sx={{ height: '16px'}}/> : <GppMaybeIcon sx={{ height: '16px'}} />}</Typography>
                 <Typography className="k-mr32">{item.mobile}</Typography>
               </div>
             </AccordionSummary>
-            <AccordionDetails className="theme__typography--secondary theme__palette--tertiary">
+            <AccordionDetails className={`theme__typography--secondary  ${item.subscribed ? 'theme__palette--tertiary' : 'theme__palette--primary'}`}>
+              <div className="k-flex">
+                <Typography className="k-flex k-aic">{item.subscribed ? <VerifiedIcon sx={{ height: '16px'}}/> : <GppMaybeIcon sx={{ height: '16px'}} />} {item.subscribed ? 'Subscribed' : 'Not subscribed'}</Typography>
+              </div>
               <div className="k-flex">
                 <Typography>Newtwork size {item.network_width}</Typography>
                 {/* <Typography className="k-pl4 k-pr4">|</Typography>
