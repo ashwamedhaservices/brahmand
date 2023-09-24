ed878d05f7f66f307585ccd091423579756716b3
Anurag Upadhyay <anuragle100@gmail.com>
2023-09-25 01:14:18 +0530

    added a fix for partnerAccount and currentUser

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Button, Container, Typography } from '@mui/material';
 import Payouts from '../components/payouts';
 import NetworkInfo from '../components/network-info';
-import { getPartnerAccounts } from '../service/ash_mlm'
+import { getPartnerAccounts, getPartnerAccountsNetwork, storageGetItem } from '../service/ash_mlm'
 import Partner_Accounts from '../_mock/partner_accounts';
 import Performers from '../components/performers';
 import ProfileHeader from '../components/profile-header';
@@ -10,10 +10,14 @@ import NetworkReport from '../components/network-report';
 
 const HomePage = () => {
   const [partnerAccount, setPartnerAccount] = useState({});
+  const [currentAccount, setCurrentAccount] = useState({});
+  const [currentAccountsNetwork, setCurrentAccountsNetwork] = useState({});
 
   useEffect(() => {
     // setPartnerAccount(() => Partner_Accounts);
     fetchPartnerAccounts();
+    fetchCurrentAccount();
+    fetchCurrentAccountsNetwork(currentAccount.id);
   }, []);
 
   const fetchPartnerAccounts = async () => {
@@ -29,11 +33,37 @@ const HomePage = () => {
     }
   }
 
-  const getNewtworkInfo = () => ({});
+  const fetchCurrentAccount = async () => {
+    console.log("fetching current user from local storage");
+    const userString = await storageGetItem('users')
+    let user;
+    if (userString != null) {
+      user = JSON.parse(userString);
+    }
+    if (user && user.id) {
+      setCurrentAccount(() => user);
+      console.log("current user is", currentAccount);
+      return
+    }
+    console.log("current user is not set");
+  }
+
+  const fetchCurrentAccountsNetwork = async (userId) => {
+    try {
+      console.log('[USER_PROFILE_PAGE]::[fetchPartnerAccountsNetwork]:: Start');
+      const accountsData = await getPartnerAccountsNetwork(userId);
+      console.log('[USER_PROFILE_PAGE]::[fetchPartnerAccountsNetwork]:: response', accountsData);
+      if (accountsData) {
+        setCurrentAccountsNetwork(() => accountsData);
+      }
+    } catch (err) {
+      console.log('[USER_PROFILE_PAGE]::[fetchPartnerAccountsNetwork]:: err', err);
+    }
+  }
 
   return (
     <Container>
-      <ProfileHeader currentUser={partnerAccount} />
+      <ProfileHeader currentUser={currentAccountsNetwork} />
       <NetworkReport></NetworkReport>
       <Payouts payouts={partnerAccount.payouts} />
       {/* <NetworkInfo network={getNewtworkInfo}/> */}
