6065dcd0d2301d6b5d579f29e245094ad2ed2d0c
Anurag Upadhyay <anuragle100gmail.com>
2024-01-29 20:16:52 +0530

    added some css fix

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -15,7 +15,16 @@ const productButtonStyle = {
   borderRadius: "12px",
   boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
   padding: "10px",
-  width: '90%'
+  width: '98%'
+}
+
+const productAccordionStyle = {
+  color: "var(--theme-font-color-secondary)",
+  backgroundColor: 'var(--theme-background-quaternary)',
+  borderRadius: "12px",
+  boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+  // padding: "10px",
+  width: '98%'
 }
 
 const ProductPage = () => {
@@ -50,22 +59,26 @@ const ProductPage = () => {
   const fullName = currentUser ? createFullName(currentUser.fname, currentUser.mname, currentUser.lname) : "Invalid Name";
   return (
     <Container>
+      <Container className='parent-container'>
       <Profile name={fullName} />
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      </Container>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '10px' }}>
         <Button variant="contained" href='/user-profile' style={{ ...productButtonStyle }}>App</Button>
         {subscriptions.map((subscription, index) => (
           <React.Fragment key={index}>
             <br />
-            <Accordion style={{ ...productButtonStyle }}>
+            <Accordion style={{ ...productAccordionStyle }}>
               <AccordionSummary expandIcon={<ExpandMoreIcon style={{ color: "var(--theme-font-color-secondary)" }} />}>
+              <div className="k-flex k-fdr k-jcsb" style={{ width: "100%" }}>
                 <Typography className="k-flex k-aic" onClick={() => subscriptionClickHandler(subscription.category)}>
                   {subscription.category.toUpperCase()}
                 <VerifiedIcon sx={{ height: '16px' }} />
                 </Typography>
+                <Typography className="k-flex k-aic">{subscription.user_category.toPascalCase()}</Typography>
+                </div>
               </AccordionSummary>
               <AccordionDetails>
-                <Typography>Name: {subscription.name}</Typography>
-                <Typography>UserCategory: {subscription.user_category}</Typography>
+                <Typography>Product Name:   {subscription.name}</Typography>
                 <Typography>Referral Count: {subscription.referral_count}</Typography>
               </AccordionDetails>
             </Accordion>
--- src/utils/default.js
+++ src/utils/default.js
@@ -38,3 +38,15 @@ export const createFullName = (firstName, middleName, lastName) => {
 
   return fullName;
 }
+
+String.prototype.toPascalCase = function() {
+  return this
+    .toLowerCase()
+    .replace(new RegExp(/[-_]+/, 'g'), ' ')
+    .replace(new RegExp(/[^\w\s]/, 'g'), '')
+    .replace(
+      new RegExp(/\s+(.)(\w*)/, 'g'),
+      ($1, $2, $3) => `${$2.toUpperCase() + $3}`
+    )
+    .replace(new RegExp(/\w/), s => s.toUpperCase());
+};
